c805f26facac39504e0107b5069af803da0956b7
ShadowStorm1218 <69068112+ShadowStorm1218@users.noreply.github.com>
2022-12-09 23:28:21 -0600

    Create LumberTycoon.lua

---
+++ LumberTycoon.lua
@@ -0,0 +1,32 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/ShadowStorm1218/Kavo_GUI/main/Kavo_UI.lua"))()
+local Window = Library.CreateLib("Vertex", "GrapeTheme")
+
+if game.PlaceId == 13822889 then
+--Player
+local Player = Window:NewTab("Player")
+local PlayerSection = Player:NewSection("Player Funtions")
+
+--VARIABLES
+local Humanoid = game:GetService("Players").LocalPlayer.Character.Humanoid;
+local playertpvar = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+
+
+
+--Speed (Slider)
+PlayerSection:NewSlider("Speed", "Adjust speed power", 500, 16, function(speed) -- 500 (MaxValue) | 0 (MinValue)
+    local Humanoid = game:GetService("Players").LocalPlayer.Character.Humanoid;
+    _G.WS = speed;
+    Humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function()
+    Humanoid.WalkSpeed = _G.WS;
+    end)
+    Humanoid.WalkSpeed = _G.WS;
+end)
+--Jump (Slider)
+PlayerSection:NewSlider("Jump", "Adust Jump Power", 500, 50, function(jump) -- 500 (MaxValue) | 0 (MinValue)
+    local Humanoid = game:GetService("Players").LocalPlayer.Character.Humanoid;
+    _G.JP = jump;
+    Humanoid:GetPropertyChangedSignal("JumpPower"):Connect(function()
+    Humanoid.JumpPower = _G.JP;
+    end)
+    Humanoid.JumpPower = _G.JP;
+end)
